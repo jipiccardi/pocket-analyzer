4a83bd22342263354778821c519376fd6804ffdc
jipiccardi <jipiccardi@gmail.com>
2025-03-09 20:21:53 -0300

    general improvments: add signal and threads

--- app/calibrate_window.py
+++ app/calibrate_window.py
@@ -1,40 +1,56 @@
+import logging
 import time
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton, QProgressDialog
-from PyQt5.QtCore import Qt, QTimer
-from conn import STX,ETX
+from itertools import count
+from multiprocessing.pool import worker
 
-from debug_window import DebugWindow
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QGridLayout, QPushButton, QProgressDialog, QProgressBar, QMessageBox
+from PyQt5.QtCore import Qt,QThread,pyqtSignal
+from PyQt5.sip import array
+
 from globals import serial_client
-from models import MeasuredValue
-
-def show_calibrate_window():
-    window = CalibrateWindow()
-    window.exec_()
+from typing import List
+from models import MeasuredValue,save_measured_values_to_csv
 
 
 class CalibrateWindow(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.progress_dialog = None
         self.setWindowTitle("Calibrate Window")
         self.setGeometry(250, 250, 1000, 600)
+
+        # Calib results
+        self.match1_data: List[MeasuredValue] = []
 
         main_layout = QVBoxLayout()
         buttons_layout = QGridLayout()
 
-        match_1_btn = QPushButton("Match 1")
-        match_1_btn.clicked.connect(lambda: self.run_calib('MTC000001'))
+        match_1_btn = QPushButton('Match 1')
+        match_1_btn.clicked.connect(lambda: self.calib_button_clicked('MTC000001'))
+
         match_2_btn = QPushButton("Match 2")
-        match_2_btn.clicked.connect(self.run_calib)
+        match_2_btn.setEnabled(False)
+        match_2_btn.clicked.connect(self.calib_button_clicked)
+
         open_1_btn = QPushButton("Open 1")
-        open_1_btn.clicked.connect(self.run_calib)
+        open_1_btn.setEnabled(False)
+        open_1_btn.clicked.connect(self.calib_button_clicked)
+
         open_2_btn = QPushButton("Open 2")
-        open_2_btn.clicked.connect(self.run_calib)
+        open_2_btn.setEnabled(False)
+        open_2_btn.clicked.connect(self.calib_button_clicked)
+
         short_1_btn = QPushButton("Short 1")
-        short_1_btn.clicked.connect(self.run_calib)
+        short_1_btn.setEnabled(False)
+        short_1_btn.clicked.connect(self.calib_button_clicked)
+
         short_2_btn = QPushButton("Short 2")
-        short_2_btn.clicked.connect(self.run_calib)
+        short_2_btn.setEnabled(False)
+        short_2_btn.clicked.connect(self.calib_button_clicked)
+
         through_btn = QPushButton("Through")
-        through_btn.clicked.connect(self.run_calib)
+        through_btn.setEnabled(False)
+        through_btn.clicked.connect(self.calib_button_clicked)
 
         buttons_layout.addWidget(match_1_btn, 0, 0)
         buttons_layout.addWidget(match_2_btn, 0, 1)
@@ -47,27 +63,70 @@ class CalibrateWindow(QDialog):
         main_layout.addLayout(buttons_layout)
         self.setLayout(main_layout)
 
-    def run_calib(self, cmd: str):
-        serial_client.send_cmd(cmd)
+    def calib_button_clicked(self, cmd: str):
+        self.progress_dialog = ProgressDialog(self)
+        task = CalibThread()
+        self.progress_dialog.set_worker_thread(task)
+        task.finished_signal.connect(self.task_completed)
+        task.start()
 
-        progress_dialog = QProgressDialog("Running task...", "Cancel", 0, 100, self)
-        progress_dialog.setWindowModality(Qt.WindowModal)
-        progress_dialog.setAutoClose(True)
-        progress_dialog.setAutoReset(True)
-        progress_dialog.show()
+        self.progress_dialog.exec_()
 
+    def task_completed(self, result: List[MeasuredValue]):
+        self.progress_dialog.accept()  # Close the dialog
+        QMessageBox.information(self, "Success", "Task completed successfully!")
+
+        self.match1_data = result
+        for d in self.match1_data:
+            d.print_value()
+        save_measured_values_to_csv('./data/match1.csv', self.match1_data)
+
+
+class ProgressDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.worker_thread = None
+        self.setWindowTitle("Processing")
+        self.setModal(True)
+        self.setFixedSize(300, 200)
+
+        self.layout = QVBoxLayout()
+        self.progress_bar = QProgressBar(self)
+        self.progress_bar.setRange(0, 0)  # Infinite progress
+        self.layout.addWidget(self.progress_bar)
+
+        self.cancel_button = QPushButton("Cancel", self)
+        self.cancel_button.clicked.connect(self.cancel_task)
+        self.layout.addWidget(self.cancel_button)
+
+        self.setLayout(self.layout)
+
+    def set_worker_thread(self, thread):
+        self.worker_thread = thread
+
+    def cancel_task(self):
+        if self.worker_thread:
+            self.worker_thread.requestInterruption()  # Gracefully stop the thread
+        self.reject()  # Close dialog
+
+
+class CalibThread(QThread):
+    finished_signal = pyqtSignal(list)
+
+    def run(self):
         data = []
-        eot = False
-        while not eot:
-            time.sleep(0.005)
-            v = serial_client.receive_value()
-
-            #if len(v) > 0 and v[0] == STX and v[-1] == ETX:
+        # eot = False
+        # while not eot:
+        for _ in range(1000):
+            if self.isInterruptionRequested():
+                return
+            time.sleep(0.00005)
+            #v = serial_client.receive_value()
+            v = b'\x02fffff2222333344445555\x03'
             if v.startswith(b'\x02') and v.endswith(b'\x03'):
                 print(v)
-                data.append(MeasuredValue(v[4:25]))
-            if 'Termine'.encode('UTF-8') in v:
-                eot = True
+                data.append(MeasuredValue(v[1:25]))
+            #if 'Termine'.encode('UTF-8') in v:
+            #    eot = True
 
-        for d in data:
-            d.print_value()
+        self.finished_signal.emit(data)
--- app/connect_window.py
+++ app/connect_window.py
@@ -1,36 +1,32 @@
 import serial.tools.list_ports
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QListWidget, QPushButton, QHBoxLayout
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QListWidget, QPushButton
+from PyQt5.QtCore import pyqtSignal
 
 from globals import serial_client
 
 
-def show_connect_window():
-    window = ConnectWindow()
-    window.exec_()
-
-
 class ConnectWindow(QDialog):
+    connection_established_signal = pyqtSignal(bool)
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setGeometry(250, 250, 500, 300)
 
         main_layout = QVBoxLayout()
-        buttons_layout = QHBoxLayout
 
         self.port_list = QListWidget()
+        self.port_list.currentItemChanged.connect(self.update_connect_button_state)
+
+        self.connect_button = QPushButton('Connect')
+        self.connect_button.setEnabled(False)
+        self.connect_button.clicked.connect(self.connect_to_selected_port)
+
         refresh_button = QPushButton('Refresh')
         refresh_button.clicked.connect(self.refresh_button)
 
-        connect_button = QPushButton('Connect')
-        connect_button.clicked.connect(lambda: serial_client.connect(self.port_list.currentItem().text()))
-
         main_layout.addWidget(self.port_list)
         main_layout.addWidget(refresh_button)
-        main_layout.addWidget(connect_button)
-        #buttons_layout.addWidget(refresh_button)
-        #buttons_layout.addWidget(connect_button)
-
-        #main_layout.addLayout(buttons_layout)
+        main_layout.addWidget(self.connect_button)
 
         self.setLayout(main_layout)
         self.refresh_button()
@@ -41,3 +37,12 @@ class ConnectWindow(QDialog):
 
         for port in ports:
             self.port_list.addItem(port.device)
+
+    def update_connect_button_state(self):
+        self.connect_button.setEnabled(self.port_list.currentItem() is not None)
+
+    def connect_to_selected_port(self):
+        if self.port_list.currentItem():
+            serial_client.connect(self.port_list.currentItem().text())
+            self.connection_established_signal.emit(True)
+            self.accept()
