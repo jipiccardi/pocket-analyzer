7fcfc75ca39e0bb7d20c87ffa92189f1e2059d18
brunorodgz <santino21.20.5@gmail.com>
2025-04-05 21:25:48 -0300

    Prueba: commit

--- app/view/matplotlib_canvas.py
+++ app/view/matplotlib_canvas.py
@@ -5,6 +5,7 @@ from matplotlib.figure import Figure
 
 class smith_chart_canvas(FigureCanvasQTAgg):
 
+
     def __init__(self, parent=None, width=14, height=16, dpi=120):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
